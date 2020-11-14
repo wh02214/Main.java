70bc4be3fab8600c0438bcd54a0034013be927a2
wh02214 <331670419@qq.com>
2020-11-14 16:16:42 +0800

    1.0

--- src/Command.java
+++ src/Command.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 
 public class Command {
     private ArrayList<Students> list = new ArrayList<Students>();
-//    Students students = new Students("a", 60, 70, 80);
+
+    public void introduce() {
+        System.out.println("=====This is students management program=====");
+        System.out.println("1.add new students massages");
+        System.out.println("2.delete a student massage");
+        System.out.println("3.show all students massage");
+        System.out.println("9.End");
+        System.out.println("what do you want to do?");
+    }
 
     public void max() {
 
--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,32 @@ public class Main {
         Scanner sc2 = new Scanner(System.in);
         Scanner sc3 = new Scanner(System.in);
         Scanner sc4 = new Scanner(System.in);
-        System.out.println("please in put the student's name:");
-        String a = sc1.next();
-        System.out.println("please input the chinese scores:");
-        int c = sc2.nextInt();
-        System.out.println("please input the math scores:");
-        int m = sc3.nextInt();
-        System.out.println("please input the English scores:");
-        int e = sc3.nextInt();
-        command.add(new Students(a, c, m, e));
-        command.show();
+        Scanner sc5 = new Scanner(System.in);
+        for (int i = 0; ; i++) {
+            command.introduce();
+            int op = sc5.nextInt();
+            switch (op) {
+                case 1: {
+                    System.out.println("please in put the student's name:");
+                    String a = sc1.next();
+                    System.out.println("please input the chinese scores:");
+                    int c = sc2.nextInt();
+                    System.out.println("please input the math scores:");
+                    int m = sc3.nextInt();
+                    System.out.println("please input the English scores:");
+                    int e = sc4.nextInt();
+                    command.add(new Students(a, c, m, e));
+                    break;
+                }
+                case 2:
+                    System.out.println("no");
+                    break;
+                case 3:
+                    command.show();
+                    break;
+                case 9:
+                    System.exit(1);
+            }
+        }
     }
 }
